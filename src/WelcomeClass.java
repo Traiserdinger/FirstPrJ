519f75a532352e83fdbf6bff2c29336a4f5fab8a
Nguyễn Đức Trung <23687791.trung@student.iuh.edu.vn>
2024-08-16 08:09:39 +0700

    FirstPrj

--- src/WelcomeClass.java
+++ src/WelcomeClass.java
@@ -1,7 +1,7 @@
 public class WelcomeClass {
     public static void main(String[] args) {
         String Name = "Hello Nguyen Duc Trung";
-        String Course = "DHHTTT19A - 420300114604";
+        String Course = "DHKTPM19B - 420300359104";
         System.out.println("Name: " + Name);
         System.out.println("Course: " + Course);
     }
